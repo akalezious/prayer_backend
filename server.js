a43d2e1a77291eb34d362a735d279b372bf66667
akalezious <yahsi.yyavuz@gmail.com>
2026-07-18 22:49:07 +0200

    Switch to better-sqlite3

--- server.js
+++ server.js
@@ -68,3 +68,7 @@ app.get('/api/prayer-times', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Prayer times backend is running!');
+});
